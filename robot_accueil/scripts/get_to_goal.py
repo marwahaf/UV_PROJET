61f89fbbdedca3cbac42d6e587ee0c9fbfd6b83a
hafsi <marwa.hafsi@etu.imt-nord-europe.fr>
2022-02-04 14:07:29 +0100

    rviz added

--- robot_accueil/scripts/get_to_goal.py
+++ robot_accueil/scripts/get_to_goal.py
@@ -21,32 +21,60 @@ class Move_to:
         rospy.Subscriber('scan', LaserScan, self.callback_laser)
         self.goal_listener = rospy.Subscriber('/move_base_simple/goal' , PoseStamped, self.callback_goal )
         rospy.Subscriber("odom", Odometry , self.go_goal)
-        self.pose_robot = Odometry()
+        self.pose_robot = PoseStamped()
+        self.map_point = PoseStamped()
         self.command_pub = rospy.Publisher(
             '/cmd_vel_mux/input/navi',
             Twist, queue_size=10)
-        rospy.Timer(rospy.Duration(0.1),self.go_goal, oneshot = False)
+        rospy.Timer(rospy.Duration(0.1),self.move_robot, oneshot = False)
     
     def callback_goal(self,data):
         #self.local_goal est published dans la map
-        # pas besoin de self.tfListener.transformPose("map", data)
+        self.robot_move_to_goal=True
         self.local_goal = data
         return self.local_goal
  
-    def go_goal(self ,data): 
-        (self.pos, self.rot ) = self.tfListener.lookupTransform('/odom', '/map', rospy.Time(0))
+    def go_goal(self ,data):
+        self.pose_robot.pose.position.x = data.pose.pose.position.x
+        self.pose_robot.pose.position.y = data.pose.pose.position.y
+        self.pose_robot.pose.position.z = data.pose.pose.position.z
+        self.pose_robot.pose.orientation.x = data.pose.pose.orientation.x
+        self.pose_robot.pose.orientation.y = data.pose.pose.orientation.y
+        self.pose_robot.pose.orientation.z = data.pose.pose.orientation.z
+        self.pose_robot.pose.orientation.w = data.pose.pose.orientation.w
+        #print(data)
+        self.pose_robot.header.frame_id = "odom"
+        self.map_point = self.tfListener.transformPose('/map', self.pose_robot)
+        
+        '''(self.pos, self.rot ) = self.tfListener.lookupTransform('/odom', '/map', rospy.Time(0))
         print(self.pos, self.rot)
         distance =  abs(math.sqrt(((self.local_goal.pose.position.x -self.pos[0])**2)+(self.local_goal.pose.position.y-self.pos[1])**2))
         angle =  math.atan2(self.local_goal.pose.position.y - self.pos[1], self.local_goal.pose.position.x - self.pos[0])      
         #print( distance , angle , abs((angle-self.rot[2])))
-        if abs(angle) > 0.9 : 
-            self.commands.angular.z = abs((angle-self.rot[2])) * 4.0
-        if distance > 0.01 :
-            if  angle > 0 : 
-                self.commands.linear.x =  distance * 0.5    
+        '''
+    
+    def move_robot(self ,data ):
+        distance =  abs(math.sqrt(((self.local_goal.pose.position.x -self.map_point.pose.position.x)**2)+(self.local_goal.pose.position.y-self.map_point.pose.position.y)**2))
+        angle =  math.atan2(self.local_goal.pose.position.y - self.map_point.pose.position.y , self.local_goal.pose.position.x - self.map_point.pose.position.x)         
+        '''if abs(angle) > 0.9 and self.robot_move_to_goal == True : 
+            self.commands.angular.z = abs((angle-self.map_point.pose.orientation.z)) * 0.01
+            if distance > 0.01 :
+                if  angle > 0 : 
+                    self.commands.linear.x =  distance * 0.5    
+                else :
+                    self.commands.linear.x =  - distance * 0.5    
+            #self.command_pub.publish(self.commands)
             else :
-               self.commands.linear.x =  - distance * 0.5    
-        #self.command_pub.publish(self.commands)
+               self.robot_move_to_goal = False
+               self.commands.linear.x =  0.0
+               self.commands.linear.y =  0.0
+               self.commands.linear.z =  0.0
+               self.commands.angular.x =  0.0
+               self.commands.angular.y =  0.0
+               self.commands.angular.z =  0.0
+            '''
+        self.commands.linear.x =  distance * 0.5    
+        self.commands.angular.z = abs((angle-self.map_point.pose.orientation.z)) * 0.01  
         self.move_command(self.commands)
 
     def callback_laser(self,data) : 
