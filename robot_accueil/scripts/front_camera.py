ceb66181e8032562c0c9e3e2c77ee79e1d8c2d5c
Gaétan <hgaga77@gmail.com>
2022-03-07 01:40:31 +0100

    ended commenting front_camera

--- robot_accueil/scripts/front_camera.py
+++ robot_accueil/scripts/front_camera.py
@@ -51,6 +51,7 @@ lstszClassName = ("background","aeroplane", "bicycle",
     "pottedplant", "sheep", "sofa", "train", "tvmonitor")
 
 
+# Class link between ROS and DNN-Model apply on camera
 class Vision:
     def __init__(self, cvAnalyser, topic= "image_raw",depth_topic = "image_raw", synchronous= True, seconds= 0.1):
         # Attributs:
@@ -61,19 +62,27 @@ class Vision:
         self.timer = None
         self.countFrames= 0
         self.start = True
-        # Subsciber:
-        self.homegoal = rospy.Subscriber('/goal/home_returned', PoseStamped, self.returnhome)
-        self.depthSub = rospy.Subscriber(depth_topic,Image,self.depthCallback)
+
+        # Subscibers:
+        self.homegoal = rospy.Subscriber('/goal/home_returned', PoseStamped, self.returnhome)   # When topic is sent to this node, some vars are reset
+        self.depthSub = rospy.Subscriber(depth_topic,Image,self.depthCallback)      # Subscribing to the "aligned_rgb_to_depth" image
+        
+        # If we want synchronous object detection and getting image camera
+        # If too many images are sent, program will be very slow, maybe using it as asynchronous get better perfs.
         if( synchronous ):
             self.subscriber = rospy.Subscriber(topic, Image, self.synchroCallback)
         else:
             self.subscriber = rospy.Subscriber(topic, Image, self.callback)
             self.timer = rospy.Timer( rospy.Duration(seconds), self.analyse)
-        # Publisher:
-        self.publisher = rospy.Publisher("analyzed/image_raw",Image, queue_size=10)
-        self.person = rospy.Publisher("/person",Marker,queue_size=1)
-        self.goal_person = rospy.Publisher("/goal/person",PoseStamped,queue_size=1)
 
+        # Publisher:
+        self.publisher = rospy.Publisher("analyzed/image_raw",Image, queue_size=10)     # Publish the analysed image to show object detected
+        self.person = rospy.Publisher("/person",Marker,queue_size=1)    # Marker with coordinates of the object detected
+        self.goal_person = rospy.Publisher("/goal/person",PoseStamped,queue_size=1)     # Goal for the robot moving with the first coords of the object detected
+
+    """ Callback method : Get the ROS image from camera and transform it to a opencv-readable image 
+        and stores it as an attribute to be reused for processing with different methods.
+    """
     def callback(self, ros_image):
         self.cv_image= None
         try:
@@ -82,44 +91,60 @@ class Vision:
         except CvBridgeError as e:
             print(e)
 
+    """ depthCallback method : Do like "callback" method, but with the "depths" image got from camera."""
     def depthCallback(self, depths):
         self.depths = self.bridge.imgmsg_to_cv2(depths,desired_encoding="passthrough")
 
+    """ pixtoangle method: 
+        convert a pixel position to the real angle of the object pointed in the camera frame
+    """
     def pixtoangle(self, size ,pix,cam_angle):
-        ang = pix * cam_angle / size ##Calc angle from 0 to f.shape
-        ang -= cam_angle/2 #Apply offset to set 0 in the middle
-        return math.radians(-ang) #-ang is for the Y values
+        ang = pix * cam_angle / size    # Calc angle from 0 to f.shape
+        ang -= cam_angle/2 # Apply offset to set 0 in the middle
+        return math.radians(-ang) #-ang is because positives Y are left and not right
 
+    """ returnhome method:
+        this is a callback method to reset the goal-sending procedure when starting the robot
+    """
     def returnhome(self, data):
         self.start = True
         print('Returned home, camera OK.')
 
+    """ analyse method :
+        uses the Analyser class to get coords of the object and calculates position in the map frame
+        an then send markers or goals.
+    """
     def analyse(self, data):
         if self.cv_image is not None and self.depths is not None :
             self.cvAnalyser.analyse()
             try:
-                img,coords = self.cvAnalyser.result()
-                self.publisher.publish(self.bridge.cv2_to_imgmsg(img, "bgr8") )
-                #calculate the distances in the map
+                img,coords = self.cvAnalyser.result()   # Get the resulted image and the coords of the detected object
+                self.publisher.publish(self.bridge.cv2_to_imgmsg(img, "bgr8") )     # publish the image to be read by Rviz or rqt_image_view
+                
+                # if there is an detected object and the distance is relevant (not 0)
                 if(coords[0]!=-1 and coords[1]!=-1) and self.depths[coords[1]][coords[0]]!=0:
                     xangle = self.pixtoangle(self.depths.shape[1],coords[0],CAMERA_ANGLE) #Get horizontal angle
                     theta = self.pixtoangle(self.depths.shape[0],coords[1],CAMERA_ANGLE_OMEGA) #Get vertical angle
-                    d = (self.depths[coords[1]][coords[0]]/1000) * math.cos(theta)
-                    x = d * math.cos(xangle)
-                    y = d * math.sin(xangle)
-                    person = Marker(
+                    d = (self.depths[coords[1]][coords[0]]/1000) * math.cos(theta)  #Get the projection of the distance in a 2D frame
+                    x = d * math.cos(xangle)    #Calculate X coord in a 2D frame
+                    y = d * math.sin(xangle)    #Calculate Y coord in a 2D frame
+                    person = Marker(            #Create a Cylinder marker with the coordinates of the object (Can be upgradable with the size of the object)
                         type = Marker.CYLINDER,
                         lifetime = rospy.Duration(0),
                         scale=Vector3(0.1, 0.1, 0.1),
                         color=ColorRGBA(0.0, 1.0, 0.0, 0.8),
                         pose = Pose(Point(x,y,0),Quaternion())
                         )
-                    person.header.frame_id = 'laserback_link'
-                    print(x,y,d, self.depths[coords[1]][coords[0]], coords[1],coords[0])
+                    person.header.frame_id = 'laserback_link'   #Setting the marker in "laserback_link" frame because there is only a Z translation between laser back and camera
+                    print(x,y,d, self.depths[coords[1]][coords[0]], coords[1],coords[0])    # Print values for debug
+                    
+                    #if we have 10 successive frames with the object
                     if self.countFrames>= 10:
-                        self.person.publish(person)
-                        print("Pub person")
+                        self.person.publish(person)     # publish topic with person
+                        print("Pub person")     #print for debug
+                        #if this is the starting of the robot procedure
                         if self.start:
+                            #Create a goal only one time
                             print("pub goal_person")
                             self.start = False
                             tmp_goal = PoseStamped()
@@ -127,17 +152,27 @@ class Vision:
                             tmp_goal.pose.position.x = x
                             tmp_goal.pose.position.y = y
                             self.goal_person.publish(tmp_goal)
+                    #if there is less than 10 successives frames with the object
                     else:
                         self.countFrames +=1
+                #if there is no object detected or the distance is not relevant
                 else:
-                    self.countFrames = 0
+                    #if there is object
+                    if(coords[0]!=-1 and coords[1]!=-1):
+                        self.countFrames = 0
             except CvBridgeError as e:
                 print(e)
 
+    """ synchroCallback method:
+        gets the image and the processing in the same time.
+    """
     def synchroCallback(self, data):
         self.callback(data)
         self.analyse(rospy.Time())
 
+""" decorateImage function :
+    decorate an image when getting localization of rects
+"""
 def decorateImage( img, lstLocs):
     for box in lstLocs:
         ClassColor = (0,0,255)
@@ -150,26 +185,33 @@ def decorateImage( img, lstLocs):
         # nom de la classe
         cv2.putText(img, "Person", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, ClassColor, 2)
 
+""" findmiddle function : 
+    finds the middle of a rect
+    function exists to have the center of the object and get the coords.
+    the "getting distance of the object procedure" can be upgraded.
+"""
 def findmiddle(x1,x2,y1,y2):
     if(x2 > x1 and y2 > y1):
         #good case
         return (int((x1+x2)/2),int((y1+y2)/2))
     else:
         print("Error finding middle : inverted rect")
-        exit()
 
-
+# Class analyser: reads the dnn model and applies it to detect objects in images
 class Analyser:
-    def __init__(self):
+
+    def __init__(self): # reads the dnn model.
         
         print("Starting analyser") 
         self.model = cv2.dnn.readNet(model=dnn_model,config=dnn_config,framework='Caffe')
         print("Analyser Started")
     
-    def __del__(self):
+    def __del__(self):  #when programm is ended
         print("End of Analyser")
 
+
     def read(self,cv_image):
+        #store the image as attribut
         self.image=cv_image
 
     def analyse(self):
@@ -222,6 +264,7 @@ class Analyser:
 
 
     def result(self):
+        #returns the image and the coords of the object.
         return self.image, self.personCoords
 
 if __name__=='__main__':
